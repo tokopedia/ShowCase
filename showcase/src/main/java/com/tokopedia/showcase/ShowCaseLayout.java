97c966d6c71686f4c685eb05b4ee2ae515167150
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-10-12 18:11:06 +0700

    Fix missing import :bug:

--- showcase/src/main/java/com/tokopedia/showcase/ShowCaseLayout.java
+++ showcase/src/main/java/com/tokopedia/showcase/ShowCaseLayout.java
@@ -14,6 +14,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.ViewUtils;
 import android.text.Html;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.TypedValue;
